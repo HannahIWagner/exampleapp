c0f343f635694ba719a52ad979bd82cfe84644c9
HannahIWagner <146005352+HannahIWagner@users.noreply.github.com>
2023-09-27 13:44:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'This is the home page'
+    return 'This is the home page!'
 
 @app.route('/about')
 def about():
